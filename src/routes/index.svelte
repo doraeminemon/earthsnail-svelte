60526961b20e71e050ff76bde092b91fb1d98f0b
Đỗ Đình Thy Sơn <tyson@pickupp.io>
2021-03-02 23:00:11 +0700

    Change to leaflet

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,138 +1,112 @@
 <script>
 	import { onMount } from 'svelte';
-	import { Map } from '@beyonk/svelte-mapbox'
+	let L
+	import MapToolbar from './MapToolbar.svelte'
 	const lng = 106.71
 	const lat = 10.76
-	const zoom = 11
+	const zoom = 13
+	let mapElement
 	let searchText
-	let mapComponent
-	onMount(() => {
-		//
+	let map
+	const initialView = [lat,lng]
+	function createMap(container) {
+	  let m = L.map(container, {preferCanvas: true }).setView(initialView, zoom);
+    L.tileLayer(
+	    'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+	    {
+	      attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
+	        &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
+	      subdomains: 'abcd',
+	    }
+	  ).addTo(m);
 
-		// create a custom style layer to implement the WebGL content
-		var highlightLayer = {
-				id: 'highlight',
-				type: 'custom',
+    return m;
+	}
 
-				// method called when the layer is added to the map
-				// https://docs.mapbox.com/mapbox-gl-js/api/#styleimageinterface#onadd
-				onAdd: function (map, gl) {
-						// create GLSL source for vertex shader
-						var vertexSource =
-								'' +
-								'uniform mat4 u_matrix;' +
-								'attribute vec2 a_pos;' +
-								'void main() {' +
-								'    gl_Position = u_matrix * vec4(a_pos, 0.0, 1.0);' +
-								'}';
+	let toolbar = {}
+	let toolbarComponent
 
-						// create GLSL source for fragment shader
-						var fragmentSource =
-								'' +
-								'void main() {' +
-								'    gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5);' +
-								'}';
+	function resizeMap() {
+	  if(map) { map.invalidateSize(); }
+  }
 
-						// create a vertex shader
-						var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-						gl.shaderSource(vertexShader, vertexSource);
-						gl.compileShader(vertexShader);
-
-						// create a fragment shader
-						var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-						gl.shaderSource(fragmentShader, fragmentSource);
-						gl.compileShader(fragmentShader);
-
-						// link the two shaders into a WebGL program
-						this.program = gl.createProgram();
-						gl.attachShader(this.program, vertexShader);
-						gl.attachShader(this.program, fragmentShader);
-						gl.linkProgram(this.program);
-
-						this.aPos = gl.getAttribLocation(this.program, 'a_pos');
-
-						// define vertices of the triangle to be rendered in the custom style layer
-						var helsinki = mapboxgl.MercatorCoordinate.fromLngLat({
-								lng: lng+0.1,
-								lat: lat+0.1,
-						});
-						var berlin = mapboxgl.MercatorCoordinate.fromLngLat({
-								lng: lng-0.1,
-								lat: lat-0.1,
-						});
-						var kyiv = mapboxgl.MercatorCoordinate.fromLngLat({
-								lng: lng+0.5,
-								lat: lat-0.3,
-						});
-
-						// create and initialize a WebGLBuffer to store vertex and color data
-						this.buffer = gl.createBuffer();
-						gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-						gl.bufferData(
-								gl.ARRAY_BUFFER,
-								new Float32Array([
-										helsinki.x,
-										helsinki.y,
-										berlin.x,
-										berlin.y,
-										kyiv.x,
-										kyiv.y
-								]),
-								gl.STATIC_DRAW
-						);
-				},
-
-				// method fired on each animation frame
-				// https://docs.mapbox.com/mapbox-gl-js/api/#map.event:render
-				render: function (gl, matrix) {
-						gl.useProgram(this.program);
-						gl.uniformMatrix4fv(
-								gl.getUniformLocation(this.program, 'u_matrix'),
-								false,
-								matrix
-						);
-						gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-						gl.enableVertexAttribArray(this.aPos);
-						gl.vertexAttribPointer(this.aPos, 2, gl.FLOAT, false, 0, 0);
-						gl.enable(gl.BLEND);
-						gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-						gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
-				}
-		};
-
-			// add the custom style layer to the map
-		console.log({ item: mapComponent })
-		mapComponent.getMap().on('load', function () {
-				mapComponent.addLayer(highlightLayer, 'building');
+  async function mapAction(container) {
+		L = await import('leaflet')
+		toolbar = L.control({ position: 'topright' })
+		toolbar.onAdd = (map) => {
+		let div = L.DomUtil.create('div');
+		toolbarComponent = new MapToolbar({
+			target: div,
+			props: {}
 		});
-		mapComponent.setCenter([lng, lat], zoom)
-		mapComponent.flyTo({ center: [lng, lat] })
+
+		toolbarComponent.$on('click-eye', ({ detail }) => eye = detail);
+		toolbarComponent.$on('click-lines', ({ detail }) => lines = detail);
+		toolbarComponent.$on('click-reset', () => {
+			map.setView(initialView, 5, { animate: true })
+		})
+
+		return div;
+	}
+
+	toolbar.onRemove = () => {
+		if(toolbarComponent) {
+			toolbarComponent.$destroy();
+			toolbarComponent = null;
+		}
+	};
+
+    map = createMap(container);
+		toolbar.addTo(map);
+
+		// markerLayers = L.layerGroup()
+ 		// for(let location of markerLocations) {
+ 		// 	let m = createMarker(location);
+		// 	markerLayers.addLayer(m);
+ 		// }
+
+		// lineLayers = createLines();
+
+		// markerLayers.addTo(map);
+		// lineLayers.addTo(map);
+    return {
+       destroy: () => {
+				 toolbar.remove();
+				 map.remove();
+				 map = null;
+			 }
+    };
+	}
+
+	onMount(async () => {
+		console.log({ mapElement })
+		await mapAction(mapElement)
 	})
 
-
 </script>
-
+<svelte:window on:resize={resizeMap} />
 <style>
-	:global(.mapboxgl-map) {
-		height: 100vh !important;
+	.map :global(.marker-text) {
+		width:100%;
+		text-align:center;
+		font-weight:600;
+		background-color:#444;
+		color:#EEE;
+		border-radius:0.5rem;
 	}
-	.search-bar {
-		left: 25px;
-		bottom: 100px;
-		z-index: 2;
-		width: calc(100vw - 50px)
+	.map :global(.map-marker) {
+		width:30px;
+		transform:translateX(-50%) translateY(-25%);
 	}
 </style>
 
 <svelte:head>
 	<title>Thông tin địa ốc</title>
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+		 integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+		 crossorigin=""/>
 </svelte:head>
 <div class="fixed search-bar p-2 bg-white rounded flex">
 	<input class="w-full" type="text" bind:value={searchText} placeholder="Tìm kiếm quận huyện" />
 </div>
-<Map
-	accessToken='pk.eyJ1IjoiZG9yYWVtaW5lbW9uIiwiYSI6ImNrbGpvMXBzaTFtc3UzMWxkNzZ4OGZvNDEifQ.6nhLO4DNHok5J17TugRB2A'
-	bind:this={mapComponent}
-	on:recentre={e => console.log(e.detail.center.lat, e.detail.center.lng) }
-	options={{ scrollZoom: false }}
-/>
+<div class="map" style="width:100vw; height:100vh" bind:this={mapElement}/>
